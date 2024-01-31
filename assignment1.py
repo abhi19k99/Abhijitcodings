bb74de18c230ef7fb7eccef28a702b5c9070f15b
abhi19k99 <157949165+abhi19k99@users.noreply.github.com>
2024-01-31 13:43:26 +0530

    Python assignment1

---
+++ assignment1.py
@@ -0,0 +1,8 @@
+name= input("Enter your name: ")
+age= input ("Enter your age: ")
+gender = input ("Enter your gender M/F: ")
+weight = input ("Enter your weight in Kg: ")
+print(name)
+print(age)
+print(gender)
+print(weight)
