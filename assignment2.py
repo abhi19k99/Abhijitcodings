bd2fdb3832892b040561cf2b7a201e9cbacef6e5
abhi19k99 <157949165+abhi19k99@users.noreply.github.com>
2024-02-08 16:24:07 +0530

    assignment2

---
+++ assignment2.py
@@ -0,0 +1,7 @@
+# calculate bmi
+
+age=int(input("Enter your age"))
+weight=float(input("Enter your weight"))
+height=float(input("Enter your height"))
+bmi=weight/(height*height)
+print("Your bmi is:",bmi)
